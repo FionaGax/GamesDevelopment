6e3dddc858c052c392fd3b19ae3a0dacf27a2690
Fiona <fionagacquin@live.co.uk>
2024-04-09 18:42:49 +0100

    Add files lfs

--- 2985181-GamesDev/Assets/Scripts/EnemyNavMeshAI.cs
+++ 2985181-GamesDev/Assets/Scripts/EnemyNavMeshAI.cs
@@ -1,20 +1,89 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
 
 public class EnemyNavMeshAI : MonoBehaviour
 {
-    public Transform player; 
+    public Transform player;
+    public Animator animator;
+    public NavMeshAgent agent;
+    public float currentVelocity;
+    public bool aggro;
+    public bool destinationReached;
+    public float destinationThreshold;
+    public Transform[] patrolPoints;
+    public float aggroTimer;
+
+    public float patrolSpeed;
+
+    public float aggroSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        aggro = false;
+        destinationReached = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Move towards the player using NavMesh
-        GetComponent<UnityEngine.AI.NavMeshAgent>().destination = player.position;
+        // Get the velocity of the enemy
+        currentVelocity = agent.velocity.magnitude;
+        // Set the speed of the animator to the velocity of the enemy
+        animator.SetFloat("velocity", currentVelocity);
+
+        if (aggro == true)
+        {
+            // Move towards the player using navmesh
+            agent.destination = player.position;
+        }
+
+        if (aggro == false && destinationReached == true)
+        {
+            destinationReached = false;
+            agent.speed = patrolSpeed;
+            // Move towards the patrol points using navmesh
+            agent.destination = patrolPoints[Random.Range(0, patrolPoints.Length)].position;
+        }
+
+        // Check if the enemy has reached the destination
+        if (Vector3.Distance(transform.position, agent.destination) < destinationThreshold)
+        {
+            destinationReached = true;
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            aggro = true;
+            agent.speed = aggroSpeed;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            agent.destination = player.position; //this is the last seen player position (since OnTriggerExit only runs once)
+            StopCoroutine("AggroTimer");
+            StartCoroutine("AggroTimer");
+        }
+    }
+
+    IEnumerator AggroTimer()
+    {
+        yield return new WaitForSeconds(aggroTimer);
+        aggro = false;
+        destinationReached = true;
     }
 }
